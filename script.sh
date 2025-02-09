04bafdc89cd666768806a8fea8c4060dd2622ba7
TheDoc14 <eyal140100@gmail.com>
2025-02-09 18:11:15 +0200

    upgraded version file with date created with the input from the user

--- script.sh
+++ script.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo "Hello, Jenkins! You provided: $1"
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+filename="output_$timestamp.html"
+echo "Hello, Jenkins! You provided: $1" | tee "$filename"
+echo "<html><body><h1>User Input Log</h1><p>$1</p></body></html>" > "$filename"
